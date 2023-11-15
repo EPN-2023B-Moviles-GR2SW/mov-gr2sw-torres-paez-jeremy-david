be7df58388fe704c953159bf89fcc704694b17e1
JeremyTorres07 <jeremy.torres@epn.edu.ec>
2023-11-15 07:50:49 -0500

    Commit Kotlin

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
+import java.util.*
+
 fun main (args: Array<String>){
     println("Hello World!")
+    // INMUTABLES (NO se reasignan"=")
+    val inmutable: String ="Adrian";
+    //inmutable = "Vicente";
+
+    //Mutables (RE asignar)
+    var mutable: String = "Vicente";
+    mutable = "Adrian";
+
+    // val > var
+    /// Duck typing
+    var ejemploVariable = "Adrian Eguez"
+    val edadEjemplo: Int = 12
+    ejemploVariable.trim()
+    // ejemploVariable = edadEjemplo
+
+    //Variable primitiva
+    val nombreProfesor:String = "Adrian Eguez"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = true
+    // Clases Java
+    val fechaNacimiento: Date = Date()
 }
